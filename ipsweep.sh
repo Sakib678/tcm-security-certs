d01c62bfbf84af8cb9f9b7c2716fa696bb35e7d0
Sakib678 <sakibarif@hotmail.co.uk>
2025-08-07 12:56:55 -0400

    Fixed errors

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 for ip in `seq 1 254 : do #for ip addresses from 1 to 254
-ping -c 192.168.197.$ip| grep "64 bytes" | cut -d " " -f 4 | tr -d ":" & 
+ping -c 1 $1.$ip| grep "64 bytes" | cut -d " " -f 4 | tr -d ":" & 
 done
-./ipsweep
+./ipsweep.sh 
 
 
-#!#Explanation of commands
+#Explanation of commands
 #grep searches for a pattern and matches it to a given one
 #the cut command extracts specific sections of data
 #the tr command uses the "-d" option to delete characters from the given input
+# the "$1" stands for the first argument that is given when the script is to be given
