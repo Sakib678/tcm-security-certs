2bb7aec0af65fdacf5651432193e1f6245b8a41e
Sakib678 <sakibarif@hotmail.co.uk>
2025-08-13 12:14:51 -0400

    Added a loop to iterate over each password in the passwordfile

--- Python101/scripts/ssh_brute.py
+++ Python101/scripts/ssh_brute.py
@@ -1,2 +1,10 @@
 from pwn import *
 import paramiko
+
+host = "127.0.0.1"
+username = "notroot"
+attempts = 0
+
+with open("best110.txt", "r") as password:
+	for password in password_list:
+		password = password.strip("\n")
